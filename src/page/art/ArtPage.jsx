44fe788596d50d37dc14bf75d6c5ed1e8a43bf19
MejiaBravoLuis <lmebravo6@gmail.com>
2025-07-16 19:04:03 -0600

    Cursos

--- src/page/art/ArtPage.jsx
+++ src/page/art/ArtPage.jsx
@@ -1,35 +1,16 @@
 import React, { useEffect } from "react";
-import { Layout, Spin, Alert, Card } from "antd";  // Usamos los componentes de Ant Design
-import { useMaterials } from "../../shared/hooks/useMaterials";  // Importamos el hook
-import { useParams } from 'react-router-dom';  // Para obtener el nombre del curso de la URL
+import { Layout, Spin, Alert, Card } from "antd"; 
 
-import { Sidebar } from "../../components/Sidebar/Sidebar";  // Sidebar
-import "./artPage.css";  // Estilos específicos de la página de Arte
+import { Sidebar } from "../../components/Sidebar/Sidebar";  
+import "./artPage.css";  
 
 export const ArtPage = () => {
-  const { courseName } = useParams();  // Obtenemos el nombre del curso desde los parámetros de la URL
-
-  // Usamos el hook para obtener los detalles del curso
-  const { course, loading, error } = useMaterials(courseName);
 
   return (
     <Layout>
       <div className="course-page-wrapper">
         <Sidebar />
-        <div className="course-main-content">
-          {loading ? (
-            <Spin size="large" />  // Muestra el spinner mientras carga
-          ) : error ? (
-            <Alert message="Error" description={error} type="error" />  // Muestra un error si ocurre
-          ) : (
-            <div>
-              <h1>{course.title}</h1>
-              <p>{course.description}</p>
-              <p><strong>Categoria:</strong> {course.category.name}</p>
-              <p><strong>Creado por:</strong> {course.createdBy.username}</p>
-            </div>
-          )}
-        </div>
+        
       </div>
     </Layout>
   );
